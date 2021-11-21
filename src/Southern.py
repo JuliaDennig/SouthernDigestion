f2a8ad0e38eab669cff4548a2f36eed305a58145
Julia Dennig <94052845+JuliaDennig@users.noreply.github.com>
2021-11-21 12:41:45 +0100

    deleted comment

--- src/Southern.py
+++ src/Southern.py
@@ -10,17 +10,3 @@ if southern_type == "cbx" or southern_type == "Cbx":
 
 elif southern_type == "in locus" or southern_type == "In locus" or southern_type == "In Locus" or southern_type == "in Locus":
     southern_in_locus()
-
-#Dateien:
-#wt - C:\Users\julia\Desktop\PythonPro\Testfiles\UMAG_04391_Genom.ape
-#del - C:\Users\julia\Desktop\PythonPro\Testfiles\del04391_GenR_Genom.ape
-
-#wt - C:\Users\julia\Desktop\PythonPro\Testfiles\04391_Genom.ape
-#del - C:\Users\julia\Desktop\PythonPro\Testfiles\04391_Genom+ GFP-Fusion.ape
-
-#wt - C:\Users\julia\Downloads\Hdp1 Lokus.ape
-#del - C:\Users\julia\Downloads\Hdp1 lokus pJU55.ape
-
-#wt - C:\Users\julia\Desktop\PythonPro\Testfiles\cbx_locus.ape
-#si - C:\Users\julia\Desktop\PythonPro\Testfiles\suc2_cbx-locus_single.ape
-#mi - C:\Users\julia\Desktop\PythonPro\Testfiles\suc2_cbx-locus_multiple.ape
